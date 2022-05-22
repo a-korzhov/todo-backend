e7e405eabaa07ac9c1a7035759db4061ef7307ba
Andrew <andrew.korzhov96@gmail.com>
2022-05-22 21:28:53 +0300

    Remove image names in db at startup

--- src/main/java/com/korzhov/todo/TodoApplication.java
+++ src/main/java/com/korzhov/todo/TodoApplication.java
@@ -1,13 +1,29 @@
 package com.korzhov.todo;
 
+import com.korzhov.todo.dao.entity.User;
+import com.korzhov.todo.dao.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootApplication
-public class TodoApplication {
+@RequiredArgsConstructor
+public class TodoApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(TodoApplication.class, args);
   }
 
+  private final UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public void run(String... args) throws Exception {
+    List<User> users = userRepository.findAll();
+    users.forEach(user -> user.setImageName(null));
+  }
 }
